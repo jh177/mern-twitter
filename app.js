dccb4b92f27aa05778294e72df1244c43cc7bac2
jh177 <jameshong177@gmail.com>
2021-12-05 21:08:04 -0800

    Set up for heroku

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ const passport = require('passport');
 const path = require('path');
 
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
+
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
